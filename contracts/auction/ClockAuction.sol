e8baa5e122c63087691b40c06f5722932ec235dc
董淑伟 <dsw0602@foxmail.com>
2018-08-22 16:29:58 +0800

    add isHuman modifier

--- contracts/auction/ClockAuction.sol
+++ contracts/auction/ClockAuction.sol
@@ -43,6 +43,10 @@ contract ClockAuction is AuctionRelated {
         rewardBox = new RewardBox(_landData, address(this), resourcesPool);
     }
 
+    modifier isHuman() {
+        require (msg.sender == tx.origin, "robot is not permitted");
+    }
+
     /// @notice This method can be used by the owner to extract mistakenly
     ///  sent tokens to this contract.
     /// @param _token The address of the token contract that you want to recover
@@ -159,7 +163,7 @@ contract ClockAuction is AuctionRelated {
 
     // TODO: advice: offer some reward for the person who claimed
     // @dev claim _tokenId for auction's lastBidder
-    function claimLandAsset(uint _tokenId) public {
+    function claimLandAsset(uint _tokenId) public isHuman {
         // Get a reference to the auction struct
         Auction storage auction = tokenIdToAuction[_tokenId];
 
