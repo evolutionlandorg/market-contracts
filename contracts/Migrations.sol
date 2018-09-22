f47257b5c0b651677a73fbcbcb69b05f18ee05ef
HackFisher <hackfisher@gmail.com>
2018-09-22 20:40:44 +0800

    move test artifacts to seperate DeployAndTest.sol, do not change the positioning of Migarate,sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,24 +1,5 @@
 pragma solidity ^0.4.23;
 
-import "@evolutionland/land/contracts/land/Atlantis.sol";
-import "evolutionlandcommon/contracts/SettingsRegistry.sol";
-import "evolutionlandcommon/contracts/StandardERC223.sol";
-import "@evolutionland/land/contracts/land/LandGenesisData.sol";
-import "@evolutionland/land/contracts/land/Atlantis.sol";
-import "@evolutionland/bancor/solidity/contracts/converter/BancorConverter.sol";
-import "@evolutionland/bancor/solidity/contracts/converter/BancorFormula.sol";
-import "@evolutionland/bancor/solidity/contracts/converter/BancorGasPriceLimit.sol";
-import "@evolutionland/bancor/solidity/contracts/token/EtherToken.sol";
-import "@evolutionland/bancor/solidity/contracts/utility/ContractFeatures.sol";
-import "@evolutionland/bancor/solidity/contracts/utility/ContractRegistry.sol";
-import "@evolutionland/bancor/solidity/contracts/utility/Whitelist.sol";
-import "@evolutionland/bancor/solidity/contracts/BancorNetwork.sol";
-import "@evolutionland/bancor/solidity/contracts/BancorExchange.sol";
-import "@evolutionland/bancor/solidity/contracts/ContractIds.sol";
-import "@evolutionland/bancor/solidity/contracts/FeatureIds.sol";
-
-
-
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
