5d8c708ca341744e115b47c680f6b8dd66e1828a
echo <hujw77@gmail.com>
2021-04-22 20:30:53 +0800

    rm flat

--- flat/IGovernorPool.sol
+++
@@ -1,8 +0,0 @@
-// Root file: contracts/auction/interfaces/IGovernorPool.sol
-
-pragma solidity ^0.4.24;
-
-contract IGovernorPool {
-    function checkRewardAvailable(address _token) external view returns(bool);
-    function rewardAmount(uint256 _amount) external; 
-}
