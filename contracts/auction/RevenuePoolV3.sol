77299d2c3e3feb1ffe44717bbb65db12ee613680
echo <hujw77@gmail.com>
2021-06-04 17:53:14 +0800

    fix gov addr missing

--- contracts/auction/RevenuePoolV3.sol
+++ contracts/auction/RevenuePoolV3.sol
@@ -65,10 +65,9 @@ contract RevenuePoolV3 is DSAuth, AuctionSettingIds {
         if (balance > 10) {
             address pointsRewardPool = registry.addressOf(AuctionSettingIds.CONTRACT_POINTS_REWARD_POOL);
             address contributionIncentivePool = registry.addressOf(AuctionSettingIds.CONTRACT_CONTRIBUTION_INCENTIVE_POOL);
-            address governorPool = registry.addressOf(CONTRACT_DIVIDENDS_POOL);
             address devPool = registry.addressOf(AuctionSettingIds.CONTRACT_DEV_POOL);
 
-            require(pointsRewardPool != 0x0 && contributionIncentivePool != 0x0 && governorPool != 0x0 && devPool != 0x0, "invalid addr");
+            require(pointsRewardPool != 0x0 && contributionIncentivePool != 0x0  && devPool != 0x0, "invalid addr");
 
             require(IERC20(_tokenAddress).transfer(pointsRewardPool, balance * 4 / 10));
             require(IERC20(_tokenAddress).transfer(contributionIncentivePool, balance * 3 / 10));
