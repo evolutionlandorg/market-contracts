6c5fdca17c131ba526b3e5c3319cfb05f88deb98
echo <hujw77@gmail.com>
2021-08-26 14:31:04 +0800

    30% farm temporarily

--- contracts/auction/RevenuePoolV4.sol
+++ contracts/auction/RevenuePoolV4.sol
@@ -14,7 +14,7 @@ import "./interfaces/IGovernorPool.sol";
  */
 
 // Use proxy mode
-contract RevenuePoolV3 is DSAuth, AuctionSettingIds {
+contract RevenuePoolV4 is DSAuth, AuctionSettingIds {
 
     bool private singletonLock = false;
 
@@ -23,7 +23,7 @@ contract RevenuePoolV3 is DSAuth, AuctionSettingIds {
 //    // 30%
 //    address public contributionIncentivePool;
 //    // 30%
-//    address public dividendsPool(temp is farm, later is gov);
+//    address public dividendsPool;
 //    // 30%
 //    address public devPool;
 
@@ -66,14 +66,19 @@ contract RevenuePoolV3 is DSAuth, AuctionSettingIds {
         if (balance > 10) {
             address pointsRewardPool = registry.addressOf(AuctionSettingIds.CONTRACT_POINTS_REWARD_POOL);
             address contributionIncentivePool = registry.addressOf(AuctionSettingIds.CONTRACT_CONTRIBUTION_INCENTIVE_POOL);
-            address farmPool = registry.addressOf(CONTRACT_DIVIDENDS_POOL);
+            address governorPool = registry.addressOf(CONTRACT_DIVIDENDS_POOL);
             address devPool = registry.addressOf(AuctionSettingIds.CONTRACT_DEV_POOL);
 
-            require(pointsRewardPool != 0x0 && contributionIncentivePool != 0x0 && farmPool != 0x0  && devPool != 0x0, "invalid addr");
+            require(pointsRewardPool != 0x0 && contributionIncentivePool != 0x0 && governorPool != 0x0  && devPool != 0x0, "invalid addr");
 
             require(IERC20(_tokenAddress).transfer(pointsRewardPool, balance * 1 / 10));
             require(IERC20(_tokenAddress).transfer(contributionIncentivePool, balance * 3 / 10));
-            require(IERC20(_tokenAddress).transfer(farmPool, balance * 3 / 10));
+
+            if (IGovernorPool(governorPool).checkRewardAvailable(_tokenAddress)) {
+                IERC20(_tokenAddress).approve(governorPool, balance * 3 / 10);
+                IGovernorPool(governorPool).rewardAmount(balance * 3 / 10);
+            }
+
             require(IERC20(_tokenAddress).transfer(devPool, balance * 3 / 10));
         }
 
